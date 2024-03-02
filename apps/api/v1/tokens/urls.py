dd2dcce844a5b133d1ab8ff61e521c51fb7fb473
Pavel Lashkov <hutjicsgo@gmail.com>
2024-03-02 17:38:14 +0300

    feat/api,users urls

--- apps/api/v1/tokens/urls.py
+++ apps/api/v1/tokens/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path("api/v1/users/login/", TokenObtainPairView.as_view(), name="api_token_auth"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
