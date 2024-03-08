6a8082e2ecd40ab9c30be8bc86b47678a23f87e3
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2024-03-08 20:37:27 +0300

    perf: optimize queryset in AmbassadorViewSet

--- apps/api/v1/ambassadors/views.py
+++ apps/api/v1/ambassadors/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Count
+
 from apps.ambassadors.models import Ambassador
 from apps.api.v1.ambassadors.serializers import (
     CreateUpdateAmbassadorSerializer,
@@ -14,7 +16,11 @@ from apps.api.v1.drf_spectacular.custom_decorators import (
 class AmbassadorViewSet(CreateListRetrieveUpdateViewSet):
     """Обработчик запросов эндпоинтов Ambassadors."""
 
-    queryset = Ambassador.objects.all().select_related("program")
+    def get_queryset(self):
+        queryset = Ambassador.objects.all().select_related("program")
+        if self.action == "list":
+            queryset = queryset.annotate(content_count=Count("contents"))
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
