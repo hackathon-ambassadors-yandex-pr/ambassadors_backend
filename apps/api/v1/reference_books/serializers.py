6f4e448a9059d18c36b9a2541be016dd60d3d609
анастасия <hutjicsgo@gmail.com>
2024-03-10 12:52:01 +0300

    feat/reference_books endpoints

--- apps/api/v1/reference_books/serializers.py
+++ apps/api/v1/reference_books/serializers.py
@@ -1,16 +1,7 @@
 from rest_framework import serializers
 
-from apps.ambassadors.choice_classes import AmbassadorStatus
 from apps.content.models import SocialNetwork
 from apps.sendings.models import Merch
-
-
-class AmbassadorStatusSerializer(serializers.ModelSerializer):
-    """Сериализатор для работы с объектом AmbassadorStatus."""
-
-    class Meta:
-        model = AmbassadorStatus
-        fields = ["id", "name"]
 
 
 class MerchSerializer(serializers.ModelSerializer):
